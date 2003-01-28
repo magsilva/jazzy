30fd082dab2160baaaba6c95da3abf93cd75cdbd
ant-roy <ant-roy>
2003-01-28 11:24:54 +0000

    Plugin updated to highlight misspelled words, and add document type specific spell checks for XML, Java and TeX modes.

--- src/com/swabunga/spell/jedit/JazzyPlugin.java
+++ src/com/swabunga/spell/jedit/JazzyPlugin.java
@@ -101,7 +101,8 @@ public class JazzyPlugin
     }
 
     String text = jta.getSelectedText();
-    int caretPosn = 0;
+    int caretPosn = 0,
+		    offset    = 0;
     boolean wholeDocument = false;
     
     if (text == null) {
@@ -109,9 +110,13 @@ public class JazzyPlugin
       caretPosn = jta.getCaretPosition();
       text = jta.getText();
       jta.selectAll();
-    }
+    }else{
+			offset = jta.getSelection()[0].getStart();
+		}
 
-    String out = jazzyChecker.checkText(text);
+    String mode = view.getBuffer().getMode().toString();
+    
+    String out = jazzyChecker.checkText(text, mode, offset);
     
     if (!out.equals(text)){ jta.setSelectedText(out);}
     if (wholeDocument){ jta.setCaretPosition(caretPosn);}
